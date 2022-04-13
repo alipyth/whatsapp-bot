beb783f670f8ad68eeda42dda4f7b845639e255f
Ali Jahani <81471730+alipyth@users.noreply.github.com>
2022-04-13 15:16:19 +0430

    main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# __________ TARFANDOON __________ #
+
+import pywhatkit
+import flask
+import requests
+
+pywhatkit.sendwhatmsg("+98", "سلام کانال ترفندون رو دنبال بکن ", 15, 3)
+
+# # ارسال یک عکس به گروه با کپشن
+# pywhatkit.sendwhats_image("AB123CDEFGHijklmn", "Images/Hello.png", "Hello")
+#
+# # ارسال عکس به مخاطب بدون کپشن
+# pywhatkit.sendwhats_image("+98", "Images/Hello.png")
+#
+# #ارسال پیام به گروه در ساعت 12:00 AM
+# pywhatkit.sendwhatmsg_to_group("AB123CDEFGHijklmn", "Hey All!", 0, 0)
+#
+# # ارسال پیام فوری به گروه
+# pywhatkit.sendwhatmsg_to_group_instantly("AB123CDEFGHijklmn", "Hey All!")
+#
+# # پخش ویدیو در یوتیوب
+# pywhatkit.playonyt("PyWhatKit")
+#
+# #این دستور در گوگل سرچ میکند
+# pywhatkit.search()
+#
+# #این دستور اطلاعات در مورد هر تاپیکی به شما میدهد
+# pywhatkit.info('' , 3)
+#
+# # این دستور نوشته را به دستخط تبدیل میکند
+# pywhatkit.text_to_handwriting('' , 'pywhatkit.png')
